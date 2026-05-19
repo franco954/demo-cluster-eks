af7dc9292c550bfc8068236612b4e57bdf2151c1
USUARIO <franconunez500@gmail.com>
2026-05-18 23:55:24 -0300

    new yaml files added

--- main.tf
+++ main.tf
@@ -168,23 +168,23 @@ provider "kubernetes" {
   }
 }
 
-resource "kubernetes_storage_class_v1" "gp2" {
-  metadata {
-    name = "gp2"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
+# resource "kubernetes_storage_class_v1" "gp2" {
+#   metadata {
+#     name = "gp2"
+#     annotations = {
+#       "storageclass.kubernetes.io/is-default-class" = "true"
+#     }
+#   }
 
-  storage_provisioner    = "ebs.csi.aws.com"
-  reclaim_policy         = "Delete"
-  volume_binding_mode    = "WaitForFirstConsumer"
-  allow_volume_expansion = true
+#   storage_provisioner    = "ebs.csi.aws.com"
+#   reclaim_policy         = "Delete"
+#   volume_binding_mode    = "WaitForFirstConsumer"
+#   allow_volume_expansion = true
 
-  parameters = {
-    type      = "gp2"
-    encrypted = "true"
-  }
+#   parameters = {
+#     type      = "gp2"
+#     encrypted = "true"
+#   }
 
-  depends_on = [aws_eks_access_policy_association.admin]
-}
+#   depends_on = [aws_eks_access_policy_association.admin]
+# }
